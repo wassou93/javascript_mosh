6fc02b57c48de23cba21f4e2ad4ebfd2817f2261
Wassim Akkari <was.akkari@gmail.com>
2024-10-03 14:21:14 +0100

    shows how calling function constructor without new will intilize the attributes in the global scope

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function Circle0(radius) {
     };
 }
 
+// Equivalent to:
 const Circle1 = new Function('radius', `
     this.radius = radius;
     this.draw = function() {
@@ -12,7 +13,9 @@ const Circle1 = new Function('radius', `
     };
 `);
 
+// Equivalent to:
 let Circle = Function.apply(Function, ['radius', `
+    console.log('this is ', this, radius);
     this.radius = radius;
     this.draw = function() {
         console.log('draw', this.radius);
@@ -22,11 +25,26 @@ let Circle = Function.apply(Function, ['radius', `
 
 // const circle1 = new Circle1(1);
 
-const circle1 = {};
-Circle.call(circle1, 0);
+const circle2 = {};
+Circle.call(circle2, 10);
 
-const circle1_2 = new Circle(12);
+const circle1 = new Circle(12);
 
+console.log(circle1)
+console.log(circle2)
+
+// If you want to declare stuff globally - not recommended
+
+const circle_g = Circle(100);
+// equivalent to:
+const circle_g_2 = Circle.call(global, 200);
+
+
+console.log(circle_g); // undefined
+console.log(circle_g_2); // undefined
+
+console.log(global.radius);
+global.draw();
 
 // circle0.draw();
-circle1.draw();
+// circle1.draw();
