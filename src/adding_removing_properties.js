e8504c3b66b57147b9fe5c1eaee6ff5c09388da2
Wassim Akkari <was.akkari@gmail.com>
2024-10-03 16:45:34 +0100

    Iterating over keys of an object

--- src/adding_removing_properties.js
+++ src/adding_removing_properties.js
@@ -21,3 +21,21 @@ delete circle.location;
 // or delete circle[propertyName]
 
 console.log(circle);
+
+// Iterating over properties of an object
+console.log("------------");
+for (key in circle) {
+    // Exclude functions
+    if (typeof circle[key] !== "function") {
+        console.log(key, ":", circle[key]);
+    }
+}
+
+// Returns an array of keys
+const keys = Object.keys(circle);
+console.log(keys);
+
+// Checking if a key exists in an object
+if ("radius" in circle) {
+    console.log("Circle has aa radius.");
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 function Circle0(radius) {
     this.radius = radius;
-    this.draw = function() {
-        console.log('draw', this.radius);
+    this.draw = function () {
+        console.log("draw", this.radius);
     };
 }
 
 // Equivalent to:
-const Circle1 = new Function('radius', `
+const Circle1 = new Function(
+    "radius",
+    `
     this.radius = radius;
     this.draw = function() {
         console.log('draw', this.radius);
     };
-`);
+`
+);
 
 // Equivalent to:
-let Circle = Function.call(Function, 'radius', `
+let Circle = Function.call(
+    Function,
+    "radius",
+    `
     console.log('this is ', this, radius);
     this.radius = radius;
     this.draw = function() {
         console.log('draw', this.radius);
     };
-`);
+`
+);
 
 // const circle1 = new Circle1(1);
 
@@ -29,15 +36,14 @@ Circle.call(circle2, 10);
 
 const circle1 = new Circle(12);
 
-console.log(circle1)
-console.log(circle2)
+console.log(circle1);
+console.log(circle2);
 
 // If you want to declare stuff globally - not recommended
 
 const circle_g = Circle(100);
 // equivalent to:
 const circle_g_2 = Circle.call(global, 200);
-
 
 console.log(circle_g); // undefined
 console.log(circle_g_2); // undefined
